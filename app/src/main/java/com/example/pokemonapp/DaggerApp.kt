986112d97d3f464f5d70055c80396ec8fb4df836
Matvey Aryka <matuxaar@gmail.com>
2023-06-05 19:37:11 +0300

    Add dagger2 dependencies

---
+++ app/src/main/java/com/example/pokemonapp/DaggerApp.kt
@@ -0,0 +1,11 @@
+package com.example.pokemonapp
+
+import android.app.Application
+import com.example.pokemonapp.di.ApplicationComponent
+import com.example.pokemonapp.di.DaggerApplicationComponent
+
+class DaggerApp : Application() {
+    val appComponent: ApplicationComponent by lazy {
+        DaggerApplicationComponent.factory().create(applicationContext)
+    }
+}
---
+++ app/src/main/java/com/example/pokemonapp/di/ApplicationComponent.kt
@@ -0,0 +1,16 @@
+package com.example.pokemonapp.di
+
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component
+interface ApplicationComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): ApplicationComponent
+    }
+}
