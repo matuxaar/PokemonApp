b96477651d17afbf3b9cb7b49255a66111282a5e
Matvey Aryka <matuxaar@gmail.com>
2023-06-05 20:21:03 +0300

    Create Modules(Network, Data, DataBase and ViewModel)

--- app/src/main/java/com/example/pokemonapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/pokemonapp/di/ApplicationComponent.kt
@@ -1,12 +1,16 @@
 package com.example.pokemonapp.di
 
 import android.content.Context
+import com.example.pokemonapp.di.modules.DataBaseModule
+import com.example.pokemonapp.di.modules.DataModule
+import com.example.pokemonapp.di.modules.NetworkModule
+import com.example.pokemonapp.di.modules.ViewModelModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component
+@Component(modules = [ViewModelModule::class, DataBaseModule::class, NetworkModule::class, DataModule::class])
 interface ApplicationComponent {
 
     @Component.Factory
---
+++ app/src/main/java/com/example/pokemonapp/di/modules/DataModule.kt
@@ -0,0 +1,7 @@
+package com.example.pokemonapp.di.modules
+
+import dagger.Module
+
+@Module
+interface DataModule {
+}
